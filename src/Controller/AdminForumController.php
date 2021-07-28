983300ea16b7cab3f69ac90f236b6255449f0c2c
Horler Raphael <hraphaelh@gmail.com>
2021-07-28 14:46:45 +0200

    [add] a lot!

---
+++ src/Controller/AdminForumController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Forum;
+use App\Form\ForumType;
+use App\Repository\ForumRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/admin/forum")
+ */
+class AdminForumController extends AbstractController
+{
+    /**
+     * @Route("/", name="admin_forum_index", methods={"GET"})
+     */
+    public function index(ForumRepository $forumRepository): Response
+    {
+        return $this->render('admin_forum/index.html.twig', [
+            'forums' => $forumRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="admin_forum_new", methods={"GET","POST"})
+     */
+    public function new(Request $request): Response
+    {
+        $forum = new Forum();
+        $forum->setCreatedAt(new \DateTime());
+        $form = $this->createForm(ForumType::class, $forum);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($forum);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('admin_forum_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('admin_forum/new.html.twig', [
+            'forum' => $forum,
+            'form' => $form,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="admin_forum_show", methods={"GET"})
+     */
+    public function show(Forum $forum): Response
+    {
+        return $this->render('admin_forum/show.html.twig', [
+            'forum' => $forum,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="admin_forum_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, Forum $forum): Response
+    {
+        $form = $this->createForm(ForumType::class, $forum);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('admin_forum_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('admin_forum/edit.html.twig', [
+            'forum' => $forum,
+            'form' => $form,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="admin_forum_delete", methods={"POST"})
+     */
+    public function delete(Request $request, Forum $forum): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$forum->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($forum);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('admin_forum_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
---
+++ src/Form/ForumType.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Forum;
+use App\Entity\Game;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ForumType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'title',
+                ]
+            ])
+            ->add('game', EntityType::class, [
+                'class' => Game::class,
+                'choice_label' => 'title',
+                'label' => 'Jeux',
+                'required' => false,
+                'attr' => [
+                    'class' => 'form-control',
+                ]
+
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Forum::class,
+        ]);
+    }
+}
