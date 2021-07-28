3c61fe861182baa9aeea3fb93c02e9e536116b26
Horler Raphael <hraphaelh@gmail.com>
2021-07-28 16:53:10 +0200

    correction mineur

--- src/Controller/ForumController.php
+++ src/Controller/ForumController.php
@@ -2,11 +2,9 @@
 
 namespace App\Controller;
 
-use App\Form\ForumType;
 use App\Repository\ForumRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -42,16 +40,5 @@ class ForumController extends AbstractController
         return $this->render('forum/index.html.twig', [
             'forum' => $forum,
         ]);
-    }
-
-    /**
-     * @Route("/admin/delete-forum/{id}", name="admin_delete_forum")
-     */
-    public function deleteForum(int $id) {
-        $post = $this->forumRepo->find($id);
-        $this->em->remove($post);
-        $this->em->flush();
-
-        return $this->redirectToRoute("forum");
     }
 }
